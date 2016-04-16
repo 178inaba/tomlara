50bba526bf8f9356070c2955eb8bd1c683ede7c5
178inaba <178inaba@users.noreply.github.com>
2016-04-16 15:13:42 +0900

    orverride bootstrap

--- src/Inaba/Tomlara.php
+++ src/Inaba/Tomlara.php
@@ -12,6 +12,43 @@ use Illuminate\Contracts\Config\Repository as RepositoryContract;
 class Tomlara extends LoadConfiguration
 {
     /**
+     * Bootstrap the given application.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function bootstrap(Application $app)
+    {
+        $items = [];
+
+        // First we will see if we have a cache configuration file. If we do, we'll load
+        // the configuration items from that file so that it is very quick. Otherwise
+        // we will need to spin through every configuration file and load them all.
+        if (file_exists($cached = $app->getCachedConfigPath())) {
+            $items = require $cached;
+
+            $loadedFromCache = true;
+        }
+
+        $app->instance('config', $config = new Repository($items));
+
+        // Next we will spin through all of the configuration files in the configuration
+        // directory and load each one into the repository. This will make all of the
+        // options available to the developer for use in various parts of this app.
+        if (! isset($loadedFromCache)) {
+            $this->loadConfigurationFiles($app, $config);
+        }
+
+        $app->detectEnvironment(function () use ($config) {
+            return $config->get('app.env', 'production');
+        });
+
+        date_default_timezone_set($config['app.timezone']);
+
+        mb_internal_encoding('UTF-8');
+    }
+
+    /**
      * load the configuration items from toml files.
      *
      * @param  \Illuminate\Contracts\Foundation\Application  $app
